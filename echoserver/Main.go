ee4e64b9745df5e5531c96c2564ac49cc65ac12f
Mark Bidewell <mbidewell@Marks-MBP-145.lan>
2017-02-05 20:25:31 -0500

    Updates to reorganize code

--- echoserver/Main.go
+++ echoserver/Main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"net"
+
+	"github.com/mbidewell/go_demos/threadpool"
 )
+
+type EchoConn struct {
+	conn net.Conn
+}
 
 func main() {
 	conn, err := net.Listen("tcp", ":9001")
 
-	tp := createThreadPool(3)
+	tp := threadpool.CreateThreadPool(3)
 
 	if err != nil {
 		fmt.Println(err.Error())
@@ -17,7 +23,21 @@ func main() {
 
 	for true {
 		incomming, _ := conn.Accept()
-		tp.processConnection(incomming)
+		cw := EchoConn{conn: incomming}
+		tp.SubmitWork(cw)
 	}
 
+}
+
+func (ec EchoConn) Execute() {
+	var inmsg = make([]byte, 255)
+
+	_, e := ec.conn.Read(inmsg)
+	if e != nil {
+		fmt.Println(e.Error())
+	} else {
+		ec.conn.Write([]byte(inmsg))
+	}
+
+	ec.conn.Close()
 }
